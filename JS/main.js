2d662f4e3ca1512684dc1872ac92d690b00dc5af
Reuben <computergeek14@gmail.com>
2018-11-17 18:29:13 -0600

    Finished Layout of Weather summary for 800X480 reso

--- JS/main.js
+++ JS/main.js
@@ -7,22 +7,33 @@ google.charts.setOnLoadCallback(drawChart);
 
 function drawChart() {
     var data = google.visualization.arrayToDataTable([
-      ['Time', 'Temp', 'Expenses'],
-      ['2004',  1000,      400],
-      ['2005',  1170,      460],
-      ['2006',  660,       1120],
-      ['2007',  1030,      540]
+      ['Day', 'Temp'],
+      ['M',  95,],
+      ['T',  65,],
+      ['W',  65,],
+      ['T',  67,],
+      ['F',  80,],
+      ['S',  75,],
+      ['S',  75,],
+      ['M',  23,],
+      ['T',  28,],
+      ['W',  30,]
     ]);
 
     var options = {
-      title: 'Company Performance',
+      title: 'Previous 10 Day Forecast',
       curveType: 'function',
       legend: { position: 'bottom' },
-      backgroundColor: '5a5a5a'
+      backgroundColor: '#FFA500',
+      vAxis: {title: 'Fahrenheit'}
     };
 
     var chart = new google.visualization.LineChart(document.getElementById('weatherChart'));
 
     chart.draw(data, options);
   }
+  $(window).resize(function(){
+    drawChart();
+  });
 });
+
