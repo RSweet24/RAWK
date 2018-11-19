12068be9a9ca9a39d7f28f5f3196502881001432
Reuben <computergeek14@gmail.com>
2018-11-18 19:51:11 -0600

    Added stock layout

--- JS/main.js
+++ JS/main.js
@@ -3,9 +3,10 @@ $(document).ready(function(){
 google.charts.load('current', {
     packages: ['corechart']
 });
-google.charts.setOnLoadCallback(drawChart);
+google.charts.setOnLoadCallback(weatherChart);
+google.charts.setOnLoadCallback(stockGraph);
 
-function drawChart() {
+function weatherChart() {
     var data = google.visualization.arrayToDataTable([
       ['Day', 'Temp'],
       ['M',  95,],
@@ -32,8 +33,32 @@ function drawChart() {
 
     chart.draw(data, options);
   }
+  function stockGraph(){
+    var stockData = google.visualization.arrayToDataTable([
+      ['Day', 'Price'],
+      ['M', 108,],
+      ['T', 105,],
+      ['W', 103,],
+      ['T', 107,],
+      ['F', 102,],
+      ['S', 98.7,],
+      ['S', 103.3,]
+    ]);
+
+    var stockOptions = {
+      title: 'Full Week stock rundown',
+      curveType: 'function',
+      legend: { position: 'bottom' },
+      backgroundColor: '#7FFF00',
+      vAxis: {title: 'Stock Price'}
+    };
+    var stockChart = new google.visualization.LineChart(document.getElementById('stockGraph'));
+    stockChart.draw(stockData, stockOptions);
+  }
+  
   $(window).resize(function(){
-    drawChart();
+    weatherChart();
+    stockGraph();
   });
 });
 
